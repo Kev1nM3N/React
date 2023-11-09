a04e4062f83cb624918bf8733c01297c4097c4bb
Kevin Mendez <mediaman502@aol.com>
2023-11-09 17:59:04 -0500

    starting netflix clone

---
+++ Week 6/my-project/app/page.tsx
@@ -0,0 +1,21 @@
+import Header from '@/components/Header'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+    <Header />
+      <main>
+        {/* Banner */}
+        <section>
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+        </section>
+      </main>
+
+      {/* Modal */}
+    </>
+  )
+}
