f51fbbe90d9117d6560a4d21234e393982ce3c1e
Kevin Mendez <mediaman502@aol.com>
2023-11-10 12:01:05 -0500

    added nav bar

--- Week 6/my-project/app/page.tsx
+++ Week 6/my-project/app/page.tsx
@@ -1,5 +1,9 @@
+"use client"
+import type { NextPage } from 'next'
 import Header from '@/components/Header'
 import Image from 'next/image'
+
+//49:57
 
 export default function Home() {
   return (
--- Week 6/my-project/components/Header.tsx
+++ Week 6/my-project/components/Header.tsx
@@ -1,7 +1,30 @@
+import { SearchIcon, BellIcon } from "@heroicons/react/solid"
+import Link from "next/link"
+import { useEffect, useState } from "react"
 
 function Header() {
+
+  const [isScrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0){
+        setScrolled(true)
+      } else{
+        setScrolled(false)
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
+
   return (
-    <header>
+    <header className={`${isScrolled && "bg-[#141414]"}`}>
         <div className="flex items-center space-x-2 md:space-x-10">
             <img
             src="https://rb.gy/ulxxee"
@@ -10,7 +33,7 @@ function Header() {
             className="cursor-pointer object-contain"
             />
 
-            <ul className="hidden space-x-4">
+            <ul className="hidden space-x-4 md:flex">
                 <li className="headerLink">Home</li>
                 <li className="headerLink">Movies</li>
                 <li className="headerLink">TV Shows</li>
@@ -19,7 +42,18 @@ function Header() {
             </ul>
         </div>
 
-        <div></div>
+        <div className="flex items-center space-x-4 text-sm font-light">
+          <SearchIcon className="hidden h-6 w-6 sm:inline"/>
+          <p className="hidden lg:inline">Kids</p>
+          <BellIcon className="h-6 w-6 sm:inline"/>
+          <Link href="/account">
+            <img
+              src="https://rb.gy/g1pwyx"
+              alt=""
+              className="cursor-pointer rounded"
+            />
+          </Link>
+        </div>
     </header>
   )
 }
