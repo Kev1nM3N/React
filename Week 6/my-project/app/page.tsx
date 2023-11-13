917542dccf6e34985266f71ffd9f7923876a6ef3
Kevin Mendez <mediaman502@aol.com>
2023-11-12 23:43:45 -0500

    finished navbar?

--- Week 6/my-project/app/page.tsx
+++ Week 6/my-project/app/page.tsx
@@ -1,25 +1,28 @@
-"use client"
-import type { NextPage } from 'next'
-import Header from '@/components/Header'
-import Image from 'next/image'
+"use client";
+import type { NextPage } from "next";
+import Header from "@/components/Header";
+import Image from "next/image";
 
-//49:57
+//52:51
 
 export default function Home() {
   return (
     <>
-    <Header />
-      <main>
-        {/* Banner */}
-        <section>
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-          {/* Row */}
-        </section>
-      </main>
+      <div className="relative h-screen bg-gradient-to-b from-gray-900/10
+      to-[#010511] lg:h-[140vh]">
+        <Header />
+        <main>
+          {/* Banner */}
+          <section>
+            {/* Row */}
+            {/* Row */}
+            {/* Row */}
+            {/* Row */}
+          </section>
+        </main>
 
-      {/* Modal */}
+        {/* Modal */}
+      </div>
     </>
-  )
+  );
 }
