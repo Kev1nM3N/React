34b71aef0915bf060ed03793487ed6872ff57c29
Kevin Mendez <mediaman502@aol.com>
2023-11-14 21:48:45 -0500

    netflixOriginals undefined

--- Week 6/my-project/app/page.tsx
+++ Week 6/my-project/app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
-import type { NextPage } from "next";
 import Header from "@/components/Header";
 import Image from "next/image";
 import Banner from "@/components/Banner";
+import requests from "@/utils/requests";
 
 //1:01:11
 
-export default function Home() {
+export default function Home({ netflixOriginals }) {
+  console.log(netflixOriginals)
   return (
     <>
       <div className="relative h-screen bg-gradient-to-b from-gray-900/10
@@ -26,4 +27,40 @@ export default function Home() {
       </div>
     </>
   );
+}
+
+export const pageFirm = async () => {
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ])
+  
+
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+    },
+  }
 }
