4c295c41a4f42b2c4c42c234f1dcb11717c29e85
Kevin Mendez <mediaman502@aol.com>
2024-04-02 11:54:37 -0400

    done with that

--- Week 6/final-prep/src/pages/Home.jsx
+++ Week 6/final-prep/src/pages/Home.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 
 function Home() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState([]);
+
+  useEffect(() => {
+    async function main(params) {
+      const { data } = await axios.get(
+        `https://jsonplaceholder.typicode.com/users`
+      );
+      console.log(data);
+      setUsers(data);
+    }
+    main();
+  }, []);
+
   return (
-    <div>Home</div>
-  )
+    <div className="container">
+      <div className="row">
+        <div className="user-list">
+          {users.map((user) => (
+            <div className="user" key={user.id}>
+              <Link to={`${user.id}`}>
+                <div className="user-card">
+                  <div className="user-card__container">
+                    <h3>{user.name}</h3>
+                    <p>
+                      <b>Email:</b> {user.email}
+                    </p>
+                    <p>
+                      <b>Phone:</b> {user.phone}
+                    </p>
+                    <p>
+                      <b>Website:</b>
+                      {user.website}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- Week 6/final-prep/src/pages/Posts.jsx
+++ Week 6/final-prep/src/pages/Posts.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -29,10 +29,17 @@ function Posts() {
     <>
       <div>
         <div className="post__search">
-          <button>← Back</button>
+          <Link to="/">
+            <button>← Back</button>
+          </Link>
           <div className="post__search--container">
             <label className="post__search--label">Search by Id</label>
-            <input type="number" value={searchId} onChange={(event) => setSearchId(event.target.value)}/>
+            <input 
+              type="number" 
+              value={searchId} 
+              onChange={(event) => setSearchId(event.target.value)}
+              onKeyDown={(event) => event.key === `Enter` && onSearch()}
+              />
             <button onClick={() => onSearch()}>Enter</button>
           </div>
         </div>
